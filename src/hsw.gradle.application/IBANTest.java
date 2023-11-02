ab13b85a910c0d381b387f0dcbcbe1e81b3918bc
Marek <marek.schulte-mesum@hsw-stud.de>
2023-11-02 11:26:52 +0100

    added java classes

---
+++ src/hsw.gradle.application/IBANTest.java
@@ -0,0 +1,17 @@
+package hsw.gradle.application;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class IBANTest {
+    @Test void IBANDE() {
+        String myIBAN = "DE68210501700012345678";
+        IBAN classUnderTest = new IBAN(myIBAN);
+        assertEquals(classUnderTest.check(myIBAN), true);
+    }
+
+    @Test void IBANWrong() {
+        String myIBAN = "ZZ68210501700012345678";
+        IBAN classUnderTest = new IBAN(myIBAN);
+        assertEquals(classUnderTest.check(myIBAN), false);
+    }
+}
